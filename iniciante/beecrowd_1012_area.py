8e1f4599395f2036d251186e243756968237042d
Rafael Medeiros <rafael.mdossantos@stone.com.br>
2025-01-22 08:46:46 -0300

    :white_check_mark: beecrowd | 1012 Área

--- iniciante/beecrowd_1012_area.py
+++ iniciante/beecrowd_1012_area.py
@@ -19,6 +19,6 @@ retangulo = a * b
 
 print(f"TRIANGULO: {triangulo:.3f}")
 print(f"CIRCULO: {circulo:.3f}")
-print(f"CIRCULO: {trapezio:.3f}")
-print(f"CIRCULO: {quadrado:.3f}")
-print(f"CIRCULO: {retangulo:.3f}")
+print(f"TRAPEZIO: {trapezio:.3f}")
+print(f"QUADRADO: {quadrado:.3f}")
+print(f"RETANGULO: {retangulo:.3f}")
