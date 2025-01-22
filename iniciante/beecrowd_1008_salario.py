86d2e3f598bc91c1470f081ecd17edebce4e6391
Rafael Medeiros <rafael.mdossantos@stone.com.br>
2025-01-22 07:05:20 -0300

    :white_check_mark: beecrowd | 1008 Salário

---
+++ iniciante/beecrowd_1008_salario.py
@@ -0,0 +1,7 @@
+employee_id = int(input())
+worked_hours = int(input())
+value = float(input())
+
+salary = worked_hours * value
+print(f"NUMBER = {employee_id}")
+print(f"SALARY = U$ {salary:.2f}")
