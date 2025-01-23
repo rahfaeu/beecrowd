0715783521b2f592ce5df1b12275d7b7280f06c9
Rafael Medeiros <rafael.mdossantos@stone.com.br>
2025-01-23 11:11:55 -0300

    :white_check_mark: beecrowd | 1018 Cédulas

--- iniciante/beecrowd_1018_cedulas.py
+++ iniciante/beecrowd_1018_cedulas.py
@@ -1,9 +1,9 @@
 valor = int(input())
-notas = [100, 50, 20, 10, 5, 2, 1]
+cedulas = [100, 50, 20, 10, 5, 2, 1]
 
 print(valor)
 
-for n in notas:
+for n in cedulas:
     unidades = valor // n
     valor %= n
     print(f"{unidades} nota(s) de R$ {n},00")
