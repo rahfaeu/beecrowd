2f504c1da811bbf924bb2635e366699efc36f099
Rafael Medeiros <rafael.mdossantos@stone.com.br>
2025-01-25 00:17:30 -0300

    :white_check_mark: beecrowd | 1036 Fórmula de Bhaskara

---
+++ iniciante/beecrowd_1036_formula_de_bhaskara.py
@@ -0,0 +1,14 @@
+from math import sqrt
+a, b, c = map(float, input().split(" "))
+
+delta = b ** 2 - 4 * a * c
+
+if a == 0 or delta < 0:
+    print("Impossivel calcular")
+    exit()
+
+r1 = (-b + sqrt(delta)) / (2 * a)
+r2 = (-b - sqrt(delta)) / (2 * a)
+
+print(f"R1 = {r1:.5f}")
+print(f"R2 = {r2:.5f}")
