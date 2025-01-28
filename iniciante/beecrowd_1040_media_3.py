7818ae034fe3a0390fbab76700755d5b7b97a126
Rafael Medeiros <rafael.mdossantos@stone.com.br>
2025-01-27 23:27:12 -0300

    :white_check_mark: beecrowd | 1040 Média 3

--- iniciante/beecrowd_1040_media_3.py
+++ iniciante/beecrowd_1040_media_3.py
@@ -5,13 +5,9 @@ media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10
 print(f"Media: {media:.1f}")
 if media < 5:
     print("Aluno reprovado.")
-    quit()
-
-if media >= 7:
+elif media >= 7:
     print("Aluno aprovado.")
-    quit()
-
-if media >= 5 and media <= 6.9:
+elif media >= 5 and media <= 6.9:
     print("Aluno em exame.")
     nota_exame = float(input())
     print(f"Nota do exame: {nota_exame:.1f}")
@@ -19,13 +15,5 @@ if media >= 5 and media <= 6.9:
     if media_final > 5:
         print("Aluno aprovado.")
     else:
-        print("Aluno aprovado.")
+        print("Aluno reprovado.")
     print(f"Media final: {media_final:.1f}")
-
-
-
-
-
-
-
-
